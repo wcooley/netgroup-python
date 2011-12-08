4a0c87258c8fbc522a88401517ca87656ed0c2cd
Wil Cooley <wcooley@pdx.edu>
2011-12-08 00:40:02 -0800

    Renamed byref, libc_name with leading underscore so pydoc hides them

--- netgroup/__init__.py
+++ netgroup/__init__.py
@@ -1,4 +1,4 @@
-"""Interface to netgroup functions, which are available on most Unix systems.
+"""Common netgroup functions that are available on most Unix systems.
 
 From the Linux man page v3.23 for *netgrent(3):
 
@@ -18,16 +18,17 @@ http://planetjoel.com/viewarticle/629/Python+NSS+netgroups+interface
 
 """
 
-from ctypes import CDLL,byref,c_char_p
+from ctypes import CDLL,c_char_p
+from ctypes import byref as _byref
 
 import sys
 
 if sys.platform.startswith('linux'):
-    libc_name = 'libc.so.6'
+    _libc_name = 'libc.so.6'
 elif sys.platform == 'sunos5':
-    libc_name = 'libc.so'
+    _libc_name = 'libc.so'
 elif sys.platform == 'darwin':
-    libc_name = 'libc.dylib'
+    _libc_name = 'libc.dylib'
 else:
     raise NotImplementedError, 'Unsupported platform: ' + sys.platform
 
@@ -40,13 +41,13 @@ def getgroup(name):
     '''
     host,user,domain = c_char_p(None),c_char_p(None),c_char_p(None)
 
-    libc = CDLL(libc_name)
+    libc = CDLL(_libc_name)
 
     libc.setnetgrent(name)
 
     try:
         groups = []
-        while libc.getnetgrent(byref(host), byref(user), byref(domain)):
+        while libc.getnetgrent(_byref(host), _byref(user), _byref(domain)):
             groups.append( (host.value,user.value,domain.value) )
         return groups
 
@@ -60,5 +61,5 @@ def innetgr(netgroup,host=None,user=None,domain=None):
     Ask whether a host/user/domain tuple is part of a netgroup
     If no host,user or domain is passed then it returns true if the netgroup exists
     '''
-    libc = CDLL(libc_name)
+    libc = CDLL(_libc_name)
     return bool(libc.innetgr(netgroup,host,user,domain))
